bc1cb370110f1d9cc080aa4aa298db7071ba5639
Alberto Ramos <shujito.desu@gmail.com>
2014-12-27 23:13:08 -0700

    trash

--- webapp/_app.js
+++ webapp/_app.js
@@ -5,6 +5,6 @@ window.$ = jQuery;
 window.bootstrap = require('bootstrap');
 // libs
 require('./_js/libs/lawnchair-0.6.1.js');
-require('./_js/libs/lawnchair-adapter-indexed-db-0.6.1.js');
 // own stuff
+//require('./_js/songs.js');
 require('./_js/routes.js');
